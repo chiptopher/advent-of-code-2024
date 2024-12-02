f5e50795d8d3c5857c54ad712e3677802514dfc1
chiptopher <chrisboyerdev@gmail.com>
2024-12-02 15:29:18 -0500

    day 2 part 1

--- AdventOfCode/day2/Main.java
+++ AdventOfCode/day2/Main.java
@@ -1,27 +1,7 @@
 package AdventOfCode.day2;
 
-import java.util.List;
-
-import AdventOfCode.BaseSolver;
-
 public class Main {
     public static void main(String[] args) {
         new Solver().solve();
-    }
-
-}
-
-class Solver extends BaseSolver {
-
-    Solver() {
-        super("AdventOfCode/day2/input.txt");
-    }
-
-    @Override
-    protected void question(List<String> fileLines) {
-        System.out.println("Contents of input.txt:");
-        for (String line : fileLines) {
-            System.out.println(line);
-        }
     }
 }
---
+++ AdventOfCode/day2/Solver.java
@@ -0,0 +1,67 @@
+package AdventOfCode.day2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import AdventOfCode.BaseSolver;
+
+class Solver extends BaseSolver {
+
+    Solver() {
+        super("AdventOfCode/day2/input.txt");
+    }
+
+    @Override
+    protected void question(List<String> fileLines) {
+        int safeLevels = 0;
+        for (String rawLevelData : fileLines) {
+            List<Integer> numbers = Arrays.stream(rawLevelData.split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            if (isFloorSafe(numbers)) {
+                safeLevels++;
+            }
+            System.out.println(rawLevelData + " is " + isFloorSafe(numbers));
+
+        }
+        System.out.println("Number of safe floors is: " + safeLevels);
+    }
+
+    private boolean isFloorSafe(List<Integer> floorData) {
+        int i = 0;
+        var previousDifference = calculateFloorDifferenceAtIndex(floorData, 0);
+        while (i < floorData.size() - 1) {
+            var difference = calculateFloorDifferenceAtIndex(floorData, i);
+            if (difference == 0) {
+                return false;
+            }
+            if (calculateIfDiectionIsChanged(previousDifference, difference)) {
+                return false;
+            }
+            if (Math.abs(difference) > 3) {
+                return false;
+            }
+
+            previousDifference = difference;
+            i++;
+        }
+
+        return true;
+    }
+
+    private int calculateFloorDifferenceAtIndex(List<Integer> floorData, int index) {
+        var current = floorData.get(index);
+        var next = floorData.get(index + 1);
+        return current - next;
+    }
+
+    private boolean calculateIfDiectionIsChanged(int previous, int current) {
+        if (previous == 0) {
+            return false;
+        }
+
+        return (previous > 0 != current > 0);
+    }
+}
